553dbfdfec140b81c5566102afbb41323cf52df4
Shriram-Ghadge <Shriram.Ghadge@ibm.com>
2022-04-20 22:20:40 +0530

    different variable format

--- localusers/localuser-1.sh
+++ localusers/localuser-1.sh
@@ -47,3 +47,66 @@ echo "message from script - ${VAR_1}s"
 ### Reassignment ###
 VAR_1="Hello, There"
 echo "$VAR_1"
+
+# VAR_2="date"
+
+# echo "message is $VAR_2"        # date
+# echo "message is ${VAR_2}"      # date
+# echo "message is $(VAR_2)"      # VAR_2: command not found
+# echo "message is $(date)"       # Wed Apr 20 12:48:11 EDT 2022  
+# echo "message is $($VAR_2)"     # Wed Apr 20 12:48:11 EDT 2022  
+# echo "message is `VAR_2`"       # VAR_2: command not found
+# echo "message is `$VAR_2`"      # Wed Apr 20 12:48:11 EDT 2022
+# echo "message is `date`"        # Wed Apr 20 12:48:11 EDT 2022
+# echo "message is '$VAR_2'"      # 'date'
+# echo "message is \"$VAR_2\""    # "date"
+
+
+### ARITHMATIC OPERATIONS ###
+
+# M-1 using - $(( ))
+
+NUM1=11
+NUM2=5
+
+echo "Sum - $(( ${NUM1} + ${NUM2} ))"   ## $(( ${NUM1} + ${NUM2} )) === $(( NUM1 + NUM2 ))
+echo "Sub - $(( NUM1-NUM2 ))"
+echo "MUL - $(( NUM1*NUM2 ))"
+echo "DIV - $(( NUM1/NUM2 ))"
+echo "MOD - $(( NUM1%NUM2 ))"
+
+NUM1=$(( NUM1+1 ))
+echo "Num1 - ${NUM1}"
+
+(( NUM1++ ))
+echo "Num1 - ${NUM1}"
+
+(( --NUM1 ))
+echo "Num1 - ${NUM1}"
+
+(( NUM1+=5 ))
+echo "Num1 - ${NUM1}"
+
+(( NUM1*=5 ))
+echo "Num1 - ${NUM1}"
+
+echo "Num1 - $((NUM1--))"
+echo "Num1 - ${NUM1}"
+
+
+# M-2 - using let
+NUM1=11
+let NUM1++
+echo "Num1 - ${NUM1}"
+
+let NUM1='9+9'
+echo "Num1 - ${NUM1}"
+
+# M-3 using expr
+
+NUM1=11
+NUM1=$(expr 7 / 4)
+echo "Num1 - ${NUM1}"
+
+NUM1=$(expr ${NUM1} + 4)
+echo "Num1 - ${NUM1}"
